99f24698f740eade6b6be6db35f19992cd0feff9
Martin Kenney <martin.kenney@kenesys.co.uk>
2019-01-10 18:42:08 +0000

    update mount script

--- scripts/mount-storage.sh
+++ scripts/mount-storage.sh
@@ -3,18 +3,24 @@
 RESOURCE_GROUP=$1
 STORAGE_ACCOUNT=$2
 CONTAINER_NAME=$3
+VM_NAME=$4
 
 STORAGE_KEY=`az storage account keys list \
     --account-name $STORAGE_ACCOUNT \
     --resource-group $RESOURCE_GROUP | jq -r '.[0].value'`
 
-sudo rpm -Uvh https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm 
-sudo yum install blobfuse -y
-sudo mkdir /mnt/resource/blobfusetmp
-sudo chown pafuser /mnt/resource/blobfusetmp
-mkdir /home/pafuser/.blobfuse
-touch /home/pafuser/.blobfuse/fuse_connection.cfg
-echo -e 'accountName $STORAGE_ACCOUNT\naccountKey $STORAGE_KEY\ncontainerName $CONTAINER_NAME' > /home/pafuser/.blobfuse/fuse_connection.cfg
-chmod 700 /home/pafuser/.blobfuse/fuse_connection.cfg
-mkdir /home/pafuser/mycontainer
-sudo blobfuse /home/pafuser/mycontainer --tmp-path=/mnt/resource/blobfusetmp  --config-file=/home/pafuser/.blobfuse/fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120
+az vm run-command invoke \
+        --resource-group $RESOURCE_GROUP \
+        --name $VM_NAME \
+        --command-id RunShellScript \
+        --scripts "sudo rpm -Uvh https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm" \
+            "sudo yum install blobfuse -y" \
+            "sudo mkdir /mnt/resource/blobfusetmp" \
+            "sudo chown azureadmin /mnt/resource/blobfusetmp" \
+            "mkdir /home/azureadmin/.blobfuse" \
+            "touch /home/azureadmin/.blobfuse/fuse_connection.cfg" \
+            "echo -e 'accountName $STORAGE_ACCOUNT\naccountKey $STORAGE_KEY\ncontainerName $CONTAINER_NAME' > /home/azureadmin/.blobfuse/fuse_connection.cfg" \
+            "chmod 700 /home/azureadmin/.blobfuse/fuse_connection.cfg" \
+            "mkdir /home/azureadmin/mycontainer" \
+            "sudo blobfuse /home/azureadmin/mycontainer --tmp-path=/mnt/resource/blobfusetmp  --config-file=/home/azureadmin/.blobfuse/fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120"
+
