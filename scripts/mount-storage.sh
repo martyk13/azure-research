e648160f797ef88372be5ce10911ad5d45cd92b8
Martin Kenney <martin.kenney@kenesys.co.uk>
2019-01-10 18:55:07 +0000

    mount updates

--- scripts/mount-storage.sh
+++ scripts/mount-storage.sh
@@ -13,14 +13,15 @@ az vm run-command invoke \
         --resource-group $RESOURCE_GROUP \
         --name $VM_NAME \
         --command-id RunShellScript \
-        --scripts "sudo rpm -Uvh https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm" \
-            "sudo yum install blobfuse -y" \
-            "sudo mkdir /mnt/resource/blobfusetmp" \
-            "sudo chown azureadmin /mnt/resource/blobfusetmp" \
+        --scripts "rpm -Uvh https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm" \
+            "yum install blobfuse -y" \
+            "mkdir /mnt/resource/blobfusetmp" \
+            "chown azureadmin /mnt/resource/blobfusetmp" \
             "mkdir /home/azureadmin/.blobfuse" \
             "touch /home/azureadmin/.blobfuse/fuse_connection.cfg" \
             "echo -e 'accountName $STORAGE_ACCOUNT\naccountKey $STORAGE_KEY\ncontainerName $CONTAINER_NAME' > /home/azureadmin/.blobfuse/fuse_connection.cfg" \
             "chmod 700 /home/azureadmin/.blobfuse/fuse_connection.cfg" \
             "mkdir /home/azureadmin/mycontainer" \
-            "sudo blobfuse /home/azureadmin/mycontainer --tmp-path=/mnt/resource/blobfusetmp  --config-file=/home/azureadmin/.blobfuse/fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120"
+            "blobfuse /home/azureadmin/mycontainer --tmp-path=/mnt/resource/blobfusetmp  --config-file=/home/azureadmin/.blobfuse/fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120" \
+            "chown -R azureadmin /home/azureadmin/mycontainer"
 
